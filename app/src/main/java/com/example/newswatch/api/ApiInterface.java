71d3b4a67d5bac530080268eeee52e87d8095f65
nainglinnhtun.cu@gmail.com <nainglinnhtun.cu@gmail.com>
2020-12-19 19:18:23 +0630

    First Commit

---
+++ app/src/main/java/com/example/newswatch/api/ApiInterface.java
@@ -0,0 +1,15 @@
+package com.example.newswatch.api;
+
+import com.example.newswatch.models.topheadline.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<News>getNews(
+            @Query("country") String country,
+            @Query("apikey") String apikey
+    );
+}
